eb1be983a7b818c195899c64195256f36c605183
Matt25969 <matt.jo.hunt@gmail.com>
2019-01-29 10:34:36 +0000

    Added WordUtil class

--- src/main/java/com/qa/persistence/repository/AccountRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountRepository.java
@@ -3,7 +3,9 @@ package com.qa.persistence.repository;
 public interface AccountRepository {
 
 	String getAllAccounts();
+	
 	String createAccount(String account);
+	
 	String deleteAccount(Long id);
 
 }
